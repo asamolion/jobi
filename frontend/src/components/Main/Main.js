2c0610f142f4adb1323df46da3cef0425e068b88
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2018-01-10 12:05:18 +0500

    Improve: add login component

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Signup from '../Signup/Signup.js';
+import Login from '../Login/Login.js';
 
 export default class Main extends React.Component {
     render() {
 	return (
 	    <Switch>
-		<Route exact path='/register' component={Signup}/>
+		<Route exact path='/register' component={Signup} />
+		<Route exact path='/login' component={Login} />
 	    </Switch>
 	);
     }
